91bca481c954ac933608db926ddea0cdf8621ab9
Andrey Ramírez <132618243+AndRM99@users.noreply.github.com>
2023-05-07 17:50:12 -0600

    Update Otp
    Se agrego las funciones del otp enviar mensaje.

---
+++ lib/utils/otp/generate_otp.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+//generates a 6-digit random number that will function as the otp
+String generateOTP() {
+  var random = Random();
+  String otp = '';
+  for (var i = 0; i < 6; i++) {
+    otp += random.nextInt(9).toString();
+  }
+  return otp;
+}
---
+++ lib/utils/otp/send_sms.dart
@@ -0,0 +1,13 @@
+import 'package:direct_sms/direct_sms.dart';
+
+//In message goes the generateOTP() function.
+//remember to save the opt in some public string, so that later You can make the comparison of whether it is correct or not.
+//number is the user's phone number.
+Future<void> sendSMS(String message, String number) async {
+  //Send text message
+  try {
+    await DirectSms().sendSms(phone: number, message: message);
+  } catch (e) {
+    throw Exception('Error al enviar SMS: ${e.toString()}');
+  }
+}
