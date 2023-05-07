523b7f4380a37d31fcbd128186b55e9d9f6963a9
mathew-gc <contact.mathew9@gmail.com>
2023-05-07 19:42:00 -0400

    update permisos de sms
    se agregaron los permisos al manifiesto y se agrego en main.dart una funcion que pregunte al usuario antes de entrar la app si acepta los permisos de enviar sms

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,31 @@ import 'package:event_fit/presentation/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Agrega esta línea
-
+  await permissionVerification(); 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
+}
+
+// This void asks the user to accept the permissions to send sms.
+Future<void> permissionVerification() async {
+  // Request the necessary permissions
+  Map<Permission, PermissionStatus> statuses = await [
+    Permission.sms,
+    Permission.phone,
+  ].request();
+
+  // Check if the necessary permits have been issued
+  if (statuses[Permission.sms] != PermissionStatus.granted ||
+      statuses[Permission.phone] != PermissionStatus.granted) {
+    throw Exception('Los permisos necesarios no fueron concedidos');
+  }
 }
 
 class MyApp extends StatelessWidget {
